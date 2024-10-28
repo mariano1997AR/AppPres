c42cffc5fe0d85c263277c777ba462fc3c3b7360
Mariano <mariano.andres1024@gmail.com>
2024-10-28 18:57:36 -0300

    fdfsf

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'C:/Users/manumiki/Documents/sistemaLogin/node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'C:/Users/manumiki/Documents/sistemaLogin/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './index.css'
 import App from './App.jsx'
 
